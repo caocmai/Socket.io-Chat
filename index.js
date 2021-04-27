9016cbcd02a324cf5809a98aac57c9ab0a1eeb46
Cao Mai <caomai@rocketmail.com>
2021-04-26 17:31:49 -0700

    Add chat notification when user connect/disconnect

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+io.on("connection", (socket) => {
+  // console.log("a user connected");
+  io.emit("user_login", "A new user has joined. Hi!")
+  socket.on("chat message", (msg) => {
+    console.log('message: ' + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+      io.emit("user_login", "A user has left. Bye!")
   });
 });
 
